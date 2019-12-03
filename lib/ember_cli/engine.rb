581ecf50e104f5a43df56407cad860a68257f769
Bradley Priest <bradleypriest@gmail.com>
2019-12-03 17:40:34 +0800

    Use `ActiveSupport.on_load` to fix zeitwerk deprecations

--- lib/ember_cli/engine.rb
+++ lib/ember_cli/engine.rb
@@ -1,10 +1,15 @@
 module EmberCli
   class Engine < Rails::Engine
     initializer "ember-cli-rails.setup" do
-      require "ember_cli/ember_controller"
       require "ember_cli/route_helpers"
 
-      ActionController::Base.helper EmberRailsHelper
+      ActiveSupport.on_load(:action_controller) do
+        require "ember_cli/ember_controller"
+      end
+
+      ActiveSupport.on_load(:action_controller_base) do
+        ActionController::Base.helper EmberRailsHelper
+      end
     end
   end
 end
