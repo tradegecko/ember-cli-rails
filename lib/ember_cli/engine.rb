20150a0cd11de79df2fb8814b5ea2d4641bfeec3
Bradley Priest <bradleypriest@gmail.com>
2019-12-24 15:44:35 +0800

    Use the after_initialize hook instead of action_controller, as action_controller doesn't work how I expected

--- lib/ember_cli/engine.rb
+++ lib/ember_cli/engine.rb
@@ -3,12 +3,12 @@ module EmberCli
     initializer "ember-cli-rails.setup" do
       require "ember_cli/route_helpers"
 
-      ActiveSupport.on_load(:action_controller) do
-        require "ember_cli/ember_controller"
-      end
-
       ActiveSupport.on_load(:action_controller_base) do
         ActionController::Base.helper EmberRailsHelper
+      end
+
+      ActiveSupport.on_load(:after_initialize) do
+        require "ember_cli/ember_controller"
       end
     end
   end
